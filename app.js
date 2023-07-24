320f74638ffe5cd503a9ec80cd73798516b8ab71
jcabs13 <jcabshear@gmail.com>
2023-07-24 16:28:35 -0600

    updated app.js with error for down server

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use(express.json()); // for parsing application/json
+// Middleware that helps us to parse JSON bodies
+app.use(bodyParser.json());
 
-app.post('/webhook', function (req, res) {
-    console.log(req.body); // this is your incoming data
-    res.send('Received the data!');
+app.post('/webhook', (req, res) => {
+    console.log(req.body);  // This will print the received JSON to your server console
+
+    // Assuming the zip code is a property in the request body
+    let zipCode = req.body.zipCode;
+
+    // Now, you can do something with the received zipCode...
+    // ...
+
+    // Remember to send a response, or else the requester will hang waiting
+    res.status(200).send('Received');
 });
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Server is running...');
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
