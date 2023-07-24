46e0f05ef6c347ff7555e42ffd50350b925fb2b7
jcabs13 <jcabshear@gmail.com>
2023-07-24 16:00:51 -0600

    simplified app.js and added oath to ignore

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  res.send('First Update')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 const express = require('express');
 const app = express();
 
