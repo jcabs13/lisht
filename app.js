2f85ee591ad30bc671096e4e36ee1fd8b6873b9e
jcabs13 <jcabshear@gmail.com>
2023-07-24 15:44:11 -0600

    Added Webhook Endpoint and brokenoath folder

--- app.js
+++ app.js
@@ -9,3 +9,17 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+const express = require('express');
+const app = express();
+
+app.use(express.json()); // for parsing application/json
+
+app.post('/webhook', function (req, res) {
+    console.log(req.body); // this is your incoming data
+    res.send('Received the data!');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Server is running...');
+});
